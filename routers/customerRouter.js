cc629aa353a92e628881848f8d55b54d140328cb
U Ahmad <UmadAhmad1928@gmail.com>
2021-04-02 20:36:29 +0500

    Feature : Added update customer profile feature

--- routers/customerRouter.js
+++ routers/customerRouter.js
@@ -5,12 +5,14 @@ const restrictTo = require('../middlewares/restrictTo');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(customerController.getAllCustomers)
-  .post(protect, restrictTo('customer'), customerController.createCustomer);
+router.route('/').get(customerController.getAllCustomers);
+// .post(protect, restrictTo('customer'), customerController.createCustomer);
 
-router.route('/:id').get(customerController.getCustomer);
+router
+  .route('/:id')
+  .get(customerController.getCustomer)
+  .patch(protect, restrictTo('customer'), customerController.updateProfile);
+
 // .patch(taskerController.updateTasker)
 // .delete(taskerController.deleteTasker);
 
