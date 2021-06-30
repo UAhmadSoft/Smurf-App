f2c422d0f8f7eca57f1268eef2b48a530d2c320e
U Ahmad <UmadAhmad1928@gmail.com>
2021-06-30 18:23:01 +0500

    Separated Auth Routes into its own Router from user router

---
+++ routers/authRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+const protect = require('../middlewares/protect');
+
+const router = express.Router();
+
+router.post('/signUp', authController.signup);
+router.post('/login', authController.login);
+
+router.route('/confirmMail/:activationLink').get(authController.confirmMail);
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/resetPassword/:resetToken').post(authController.resetPassword);
+
+//router.use(protect); //  protect all router which are comming after this middleware
+
+router.patch('/updatePassword', protect, authController.updatePassword);
+router.patch('/deactivateAccount', protect, authController.deactivateAccount);
+
+module.exports = router;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,18 +5,6 @@ const protect = require('../middlewares/protect');
 
 const router = express.Router();
 
-router.post('/signUp', authController.signup);
-router.post('/login', authController.login);
-
-router.route('/confirmMail/:activationLink').get(authController.confirmMail);
-router.route('/forgotPassword').post(authController.forgotPassword);
-router.route('/resetPassword/:resetToken').post(authController.resetPassword);
-
-//router.use(protect); //  protect all router which are comming after this middleware
-
-router.patch('/updatePassword', protect, authController.updatePassword);
-router.patch('/deactivateAccount', protect, authController.deactivateAccount);
-
 router
   .route('/')
   .get(userController.getAllUsers)
